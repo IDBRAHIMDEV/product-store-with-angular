5e4c5ea1ed312ff9ca1cd0a39664885ce48b18ab
Mohamed IDBRAHIM <idbrahimdev@gmail.com>
2019-02-10 16:50:38 +0100

    final

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Router } from '@angular/router';
 import { AuthService } from './../../services/auth.service';
 import { Component, OnInit } from '@angular/core';
+import { FlashMessagesService } from 'angular2-flash-messages';
 
 @Component({
   selector: 'app-login',
@@ -11,15 +12,26 @@ export class LoginComponent implements OnInit {
 
   email = "";
   password = "";
-  constructor(private authService: AuthService, private router: Router) { }
+  constructor(private flashMessage: FlashMessagesService, private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
   }
 
   signIn() {
     this.authService.login(this.email, this.password)
-        .then((res) => this.router.navigate(['/']))
-        .catch((err) => console.error(err))
+        .then((res) => {
+          this.flashMessage.show("Welcome to your Account", {
+            cssClass: "alert-success",
+            timeout: 3000 
+          })
+          this.router.navigate(['/'])
+        })
+        .catch((err) => {
+          this.flashMessage.show(err, {
+            cssClass: "alert-danger",
+            timeout: 3000 
+          })
+        })
   }
 
   signInGoogle() {
